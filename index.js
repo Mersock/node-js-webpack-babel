a81f451d9a472bffa3fbc33fb976679c2651d6da
Phumthawan <knzphumthawan@gmail.com>
2019-11-02 15:57:22 +0700

    add readme

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express'
 const app = express();
 import routes  from './src/routes'
 
-let port = 3400
+let port = 3000
 
 app.listen(port,() => {
     console.log(`server is started and listen on port ${port}`)
